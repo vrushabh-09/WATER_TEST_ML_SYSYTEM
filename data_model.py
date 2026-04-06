2547d7ef41703b48af1fe64b6e2ece4250957926
Vrushabh Patil <vrushabhpatil97711@gmail.com>
2026-04-06 19:20:38 +0530

    Refactor Water model by removing field constraints
    Removed constraints from Water model fields.

--- data_model.py
+++ data_model.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 class Water(BaseModel):
-    ph: float = Field(..., ge=0, le=14)
-    Hardness: float = Field(..., ge=0)
-    Solids: float = Field(..., ge=0)
-    Chloramines: float = Field(..., ge=0)
-    Sulfate: float = Field(..., ge=0)
-    Conductivity: float = Field(..., ge=0)
-    Organic_carbon: float = Field(..., ge=0)
-    Trihalomethanes: float = Field(..., ge=0)
-    Turbidity: float = Field(..., ge=0)
+    ph : float
+    Hardness : float
+    Solids : float
+    Chloramines : float
+    Sulfate : float
+    Conductivity : float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity : float
